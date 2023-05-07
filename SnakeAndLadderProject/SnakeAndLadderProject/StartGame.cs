b91665d8104faf8a4e7f942e72afaff693cb670b
ronitshinde <rony.shinde753@gmail.com>
2023-05-07 12:15:00 +0530

    [Ronit] Add : UC1-Start game at position zero

---
+++ SnakeAndLadderProject/SnakeAndLadderProject/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProject
+{
+    public class StartGame
+    {
+        int startPosition = 0;
+        public void SinglePlayerGame()
+        {
+            Console.WriteLine($"Start the game at position : " + startPosition);
+        }
+    }
+}
