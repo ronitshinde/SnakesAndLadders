e767ab4a8e3d53b276ffd8a16d24c79f52eca066
ronitshinde <rony.shinde753@gmail.com>
2023-05-09 18:57:25 +0530

    [Ronit] Add : UC4-Code to check the winning position in the game

--- SnakeAndLadderProject/SnakeAndLadderProject/StartGame.cs
+++ SnakeAndLadderProject/SnakeAndLadderProject/StartGame.cs
@@ -8,30 +8,42 @@ namespace SnakeAndLadderProject
 {
     public class StartGame
     {
-        int startPosition = 0;
-        int playerPosition = 0;
+
         public void SinglePlayerGame()
         {
-            Random dieValue = new Random();
-            int dieCount = dieValue.Next(1, 7);
-            Console.WriteLine($"Die count after rolling die : " + dieCount);
-            Random moveNumber = new Random();
-            int moveOption = moveNumber.Next(0, 3);
-            Console.WriteLine("Move Option : " + moveOption);
-            switch (moveOption)
+            int startPosition = 0;
+            int winningPosition = 100;
+            int playerPosition = 0;
+
+            while (playerPosition < winningPosition)
             {
-                case 0:
-                    playerPosition = playerPosition;
-                    Console.WriteLine($"No play - player stays in same position : " + playerPosition);
-                    break;
-                case 1:
-                    playerPosition += dieCount;
-                    Console.WriteLine($"Moves ahead - player moves ahead with the die count value : " + playerPosition);
-                    break;
-                case 2:
-                    playerPosition -= dieCount;
-                    Console.WriteLine($"Moves back - player moves back with the die count value : " + playerPosition);
-                    break;
+                if (playerPosition < 0)
+                    playerPosition = 0;
+                //else
+                Console.WriteLine("PlayerPosition : " + playerPosition);
+                Random dieValue = new Random();
+                int dieCount = dieValue.Next(1, 7);
+                Console.WriteLine($"Die count after rolling die : " + dieCount);
+                Random moveNumber = new Random();
+                int moveOption = moveNumber.Next(0, 3);
+                Console.WriteLine("Move Option : " + moveOption);
+                switch (moveOption)
+                {
+                    case 0:
+                        playerPosition = playerPosition;
+                        Console.WriteLine($"No play - player stays in same position : " + playerPosition);
+                        break;
+                    case 1:
+                        playerPosition += dieCount;
+                        if(playerPosition > winningPosition)
+                            playerPosition -= dieCount;
+                        Console.WriteLine($"Moves ahead - player moves ahead with the die count value : " + playerPosition);
+                        break;
+                    case 2:
+                        playerPosition -= dieCount;
+                        Console.WriteLine($"Moves back - player moves back with the die count value : " + playerPosition);
+                        break;
+                }
             }
         }
     }
